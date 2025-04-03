961fefd8cf8fa24f5b2d04337a51dd73d6ed5399
u4ia-abhra <abhrajitgh@gmail.com>
2025-04-03 22:56:51 +0530

    updated requirements.txt

--- generation.py
+++ generation.py
@@ -3,7 +3,6 @@ import vector
 
 # Configure Gemini API
 genai.configure(api_key="AIzaSyDG5UnxQXW6JFUaLPQiGWiJ9w6ezKDHDwc")
-
 def generate_response(query):
     retrieved_docs = vector.retrieve_docs(query)
     context = "\n".join(retrieved_docs)
