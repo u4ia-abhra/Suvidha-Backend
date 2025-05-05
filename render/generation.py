2c308d18bfa560e6379ded74f4d964107fe83a37
u4ia-abhra <abhrajitgh@gmail.com>
2025-05-05 14:01:57 +0530

    Update generation.py

--- render/generation.py
+++ render/generation.py
@@ -7,16 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # Configure Gemini API
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-def generate_response(domain,query):
-    retrieved_docs = vector.retrieve_docs(domain,query)
-    context = "\n".join(retrieved_docs)
-
-    prompt = f"""You are a customer support assistant. Use the following knowledge base to answer the query:
-    {context}
-
-    User: {query}
-    Assistant:"""
-
-    response = genai.GenerativeModel("gemini-1.5-flash").generate_content(prompt)
-
+def generate_response(domain, prompt):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
     return response.text
