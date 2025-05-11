14356bacc0dd74a1cdcea8a98376502e55b7197a
u4ia-abhra <abhrajitgh@gmail.com>
2025-05-11 21:08:02 +0530

    Update app.py

--- render/app.py
+++ render/app.py
@@ -70,4 +70,6 @@ def ping():
     return "Server is live!", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
